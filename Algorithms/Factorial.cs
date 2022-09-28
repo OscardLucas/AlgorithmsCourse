af9baa4f7bc6339ff409f547745bcf05464592ed
OscardLucas <oscar.lucas@brightmachines.com>
2022-09-28 13:47:42 -0500

    Factorial class added

---
+++ Algorithms/Factorial.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public static class Factorial
+    {
+        /// <summary>
+        /// Get factorial value by recursive method
+        /// </summary>
+        /// <param name="Number"></param>
+        /// <returns></returns>
+        public static long GetFactorialRecursive(int Number)
+        {
+            if (Number < 1)
+                throw new Exception("Number must be greater than zero");
+
+            //Base
+            if (Number == 1)
+                return Number;
+
+            return Number * GetFactorialRecursive(Number - 1);
+        }
+
+        /// <summary>
+        /// Get factorial value by iterative method
+        /// </summary>
+        /// <param name="Number"></param>
+        /// <returns></returns>
+        public static long GetFactorialIterative(int Number)
+        {
+            if (Number < 1)
+                throw new Exception("Number must be greater than zero");
+
+            int result = 1;
+
+            for (int i = 1; i <= Number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberToTest = 9000;
+
+
+            Stopwatch sw1 = new Stopwatch();
+            Stopwatch sw2 = new Stopwatch();
+
+            sw1.Start();
+            long result = Factorial.GetFactorialRecursive(NumberToTest);
+            sw1.Stop();
+
+            sw2.Start();
+            long result2 = Factorial.GetFactorialIterative(NumberToTest);
+            sw2.Stop();
+            Console.WriteLine($"Time elapsed for recursive method: {sw1.Elapsed.TotalMilliseconds}");
+            Console.WriteLine($"Time elapsed for iterative method: {sw2.Elapsed.TotalMilliseconds}");
+
+        }
+
+
+
+    }
+}
