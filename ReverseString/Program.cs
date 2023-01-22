c608a9570bd9b03ca76f63e3361ae6bca64655e6
oskr_ <oskr_@LAPTOP-AS15LFEE>
2023-01-22 00:20:13 -0600

    Reverse string problem

---
+++ ReverseString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Ho";
+
+            string result = ReverseString.Reverse(message);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ReverseString/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseString
+{
+    public static class ReverseString
+    {
+        public static string Reverse(string str)
+        {
+            int length = str.Length;
+
+            if (length <= 1)
+                return str;
+
+            return str[length - 1] + Reverse(str.Remove(length-1));
+
+        }
+
+
+    }
+}
