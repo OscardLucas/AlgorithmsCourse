9ab155e3ef353634c0485fbdb11b9db9f8c84bb4
oskr_ <oskr_@LAPTOP-AS15LFEE>
2023-01-21 23:23:19 -0600

    Fibonacci solution

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fibonacci
+{
+    public static class Fibonacci
+    {
+
+        //Fibonacci
+        //0 1 1 2 3 5 8 13 21 34 ...
+
+        public static int GetIterative(int index)
+        {
+            int prev1 = 0;
+            int prev2 = 0;
+
+            for (int i = 0; i <= index; i++)
+            {
+                if (i == 0) continue;
+
+                if (i == 1 || i == 2)
+                {
+                    prev1 = 1;
+                }
+
+                else
+                {
+                    int temp = prev2;
+                    prev2 = prev1;
+                    prev1 = temp + prev2;
+
+                }
+            }
+
+            return prev1 + prev2;
+        }
+
+        public static int GetRecursive(int index)
+        {
+            if (index < 2)
+                return index;
+
+            return GetRecursive(index - 1) + GetRecursive(index - 2);
+        }
+
+
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int indexFibonacci = 8;
+
+            int result = Fibonacci.GetRecursive(indexFibonacci);
+
+            Console.WriteLine($"Recursive result : {result}");
+
+            result = Fibonacci.GetIterative(indexFibonacci);
+
+            Console.WriteLine($"Iterative result : {result}");
+
+        }
+    }
+}
