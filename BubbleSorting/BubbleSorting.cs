b9b7f5ed1f537a8559a6a4222c6c4e81369df8f6
Oscar Lucas <oscar.lucas@brightmachines.com>
2023-01-29 01:43:32 -0600

    BubleSorting

---
+++ BubbleSorting/BubbleSorting.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSorting
+{
+    public static class BubbleSorting
+    {
+
+        public static void Sort(this int [] array)
+        {
+            int length = array.Length;
+
+            int k = 0; //Iterations
+
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length - 1; j++)
+                {
+                    Console.WriteLine($"Iteration number {k++}");
+                    //Swap
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        public static void SortWithSwapFlag(this int[] array)
+        {
+            int length = array.Length;
+
+            int k = 0; //Iterations
+
+            bool swap = false;
+
+            //[7, 3, 2, 4]
+            for(int i = length; i > 0; i--)
+            {
+                for(int j = 0; j < i-1; j++)
+                {
+                    Console.WriteLine($"Iteration number {k++}");
+                    //Swap
+                    if(array[j] > array[j + 1])
+                    {
+                        swap = true;
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+
+                if (!swap) break;
+                else swap = false;
+            }
+        }
+    }
+}
---
+++ BubbleSorting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BubbleSorting
+{
+    class Program
+    //One of the simpliest but one of the least efficient
+    //Space copmplexity O(1)
+    //Time complexity O(N^2)
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[] { 1, 2, 3 ,4 ,5,7,9};
+
+            myArray.SortWithSwapFlag();
+
+            myArray.ToList().ForEach(x => Console.WriteLine(x));
+
+
+            int[] myArray2 = new int[] { 1, 2, 3, 4, 5 ,7,9};
+
+            myArray2.Sort();
+
+            myArray2.ToList().ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
